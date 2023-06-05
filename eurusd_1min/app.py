545cd65fab5c4d80c63703cec7676082629a5b47
Joel Chan <joelchanzhiyang@gmail.com>
2023-06-05 22:32:51 +0800

    Add the app file

--- eurusd_1min/app.py
+++ eurusd_1min/app.py
@@ -11,8 +11,31 @@ filenames = ["eurusd_1min_data1.txt", "eurusd_1min_data2.txt", "eurusd_1min_data
 #     out_file_path = clean_json_prefix + filenames[2]
 #     with open(out_file_path, "w") as file2:
 #         json.dump(data, file2)
-    
 
-with open(clean_json_prefix+filenames[2]) as file:
-    k = json.load(file)
-    print(len(k))
+store = []
+
+dates = set()
+
+# for filename in filenames:
+#     with open(clean_json_prefix+filename) as file:
+#         k = json.load(file)
+#         store = store + k
+
+
+repeated_dates = []
+store2 = []
+
+for row in store:
+    date = row["date"]
+    if date not in dates:
+        dates.add(date)
+        store2.append(row)
+    else:
+        print("Fuck!")
+        repeated_dates.append(date)
+
+print(len(store))
+print(len(store2))
+
+with open("combined_repeated_removed/"+"combined.json", "w") as file3:
+    json.dump(store2, file3)
